f83b869ec2f1e6bc557cef11ff6ef1e1b48e54dc
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-06-23 16:46:37 +0530

    Update afk.py

--- VegetaRobot/modules/afk.py
+++ VegetaRobot/modules/afk.py
@@ -145,6 +145,15 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
+__mod_name__ = "AFK"
+
+
+__help__ = """
+  - /afk <reason>: Mark yourself as AFK.
+  - brb <reason>: Same as the afk command, but not a command.\n
+  When marked as AFK, any mentions will be replied to with a message stating that you're not available!
+"""
+
 __command_list__ = ["afk"]
 __handlers__ = [(AFK_HANDLER, AFK_GROUP), (AFK_REGEX_HANDLER, AFK_GROUP),
                 (NO_AFK_HANDLER, AFK_GROUP),
