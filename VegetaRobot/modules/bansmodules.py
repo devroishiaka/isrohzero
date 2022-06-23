dcc8552f26ccccadccacc767e06ba4d0593208ed
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2022-06-23 13:17:20 +0530

    Update bansmodules.py

--- VegetaRobot/modules/bansmodules.py
+++ VegetaRobot/modules/bansmodules.py
@@ -1,20 +1,8 @@
-__mod_name__ = "Bᴀɴ-Mᴜᴛᴇ"
+__mod_name__ = "Mᴜᴛᴇ"
 
 __help__ = """
- ✪︎ `/punchme`*:* punches the user who issued the command.
- ✪︎ `/kickme`*:* kicks the user who issued the command.\n
-*Muting a user commands:*
- ✪︎ `/mute <userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
- ✪︎ `/tmute <userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- ✪︎ `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.\n
-*ban a user commands:*
- ✪︎ `/ban <userhandle>`*:* bans a user. (via handle, or reply)
- ✪︎ `/sban <userhandle>`*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
- ✪︎ `/tban <userhandle> x(m/h/d)`*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- ✪︎ `/unban <userhandle>`*:* unbans a user. (via handle, or reply)
- ✪︎ `/kick <userhandle>`*:* Kicks a user out of the group, (via handle, or reply)
- ✪︎ `/punch <userhandle>`*:* Punches a user out of the group, (via handle, or reply)\n
- *ban channel commands:*
-  ✪︎ `/cban` or `/channelban` ban a channel. 
-  ✪︎ `/uncban` or `/channelunban`unban channel. 
+*Admins only:*
+ • `/mute <userhandle> <reason>(optional)`*:* silences a user. Can also be used as a reply, muting the replied to user.
+ • `/tmute <userhandle> x(m/h/d) <reason>(optional)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+ • `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
 """
