57d66d1d80cd8224f443ca25cc4076bb11b48149
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-06-23 16:49:31 +0530

    Create mutingmod.py

---
+++ VegetaRobot/modules/mutingmod.py
@@ -0,0 +1,8 @@
+__mod_name__ = "Mᴜᴛᴇ"
+
+__help__ = """
+*Admins only:*
+ • `/mute <userhandle> <reason>(optional)`*:* silences a user. Can also be used as a reply, muting the replied to user.
+ • `/tmute <userhandle> x(m/h/d) <reason>(optional)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+ • `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+"""
