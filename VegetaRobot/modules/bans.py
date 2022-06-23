3443048fbbbc270b2dd920863677d80661d5731c
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2022-06-23 13:12:09 +0530

    Update bans.py

--- VegetaRobot/modules/bans.py
+++ VegetaRobot/modules/bans.py
@@ -516,7 +516,22 @@ UNBAN_BUTTON_HANDLER = CallbackQueryHandler(unbanb_btn, pattern=r"unbanb_")
 ROAR_HANDLER = CommandHandler("roar", selfunban)
 PUNCHME_HANDLER = DisableAbleCommandHandler(["punchme", "kickme"], punchme, filters=Filters.group)
 
+__mod_name__ = "Bᴀɴs"
 
+__help__ = """
+ ✪︎ `/punchme`*:* punches the user who issued the command.
+ ✪︎ `/kickme`*:* kicks the user who issued the command.\n
+*ban a user commands:*
+ ✪︎ `/ban <userhandle>`*:* bans a user. (via handle, or reply)
+ ✪︎ `/sban <userhandle>`*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
+ ✪︎ `/tban <userhandle> x(m/h/d)`*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+ ✪︎ `/unban <userhandle>`*:* unbans a user. (via handle, or reply)
+ ✪︎ `/kick <userhandle>`*:* Kicks a user out of the group, (via handle, or reply)
+ ✪︎ `/punch <userhandle>`*:* Punches a user out of the group, (via handle, or reply)\n
+ *ban channel commands:*
+  ✪︎ `/cban` or `/channelban` ban a channel. 
+  ✪︎ `/uncban` or `/channelunban`unban channel. 
+"""
 
 dispatcher.add_handler(BAN_HANDLER)
 
