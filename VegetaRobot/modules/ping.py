9f28e45cd25be97c3a826eb30a82dc7b3faa0d33
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-06-26 17:41:44 +0530

    Update ping.py

--- VegetaRobot/modules/ping.py
+++ VegetaRobot/modules/ping.py
@@ -9,7 +9,7 @@ from VegetaRobot import StartTime, dispatcher
 from VegetaRobot.modules.helper_funcs.chat_status import sudo_plus
 from VegetaRobot.modules.disable import DisableAbleCommandHandler
 
-PING_IMG="https://telegra.ph/file/7f09ce39158cf595779b1.jpg"
+PING_IMG="https://te.legra.ph/file/f47cc3d25cc70fee5c929.jpg"
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
