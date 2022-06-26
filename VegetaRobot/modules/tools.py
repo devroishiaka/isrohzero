c8e406854956775f99da367202c5fc13a82c60ec
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-06-26 18:53:27 +0530

    Update tools.py

--- VegetaRobot/modules/tools.py
+++ VegetaRobot/modules/tools.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "🎒Tools"
+__mod_name__ = "Tᴏᴏʟs"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
