c33dbe550d1c16d7296ba1bd8d06bf43328b130b
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-06-28 06:53:11 +0530

    Update welcome.py

--- VegetaRobot/modules/welcome.py
+++ VegetaRobot/modules/welcome.py
@@ -37,8 +37,8 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import escape_markdown, mention_html, mention_markdown
 
-OWNER_IMG= "https://telegra.ph/file/c05d10b9508e92e9560ec.jpg"
-VEGETA_ADD_IMG="https://telegra.ph/file/a8f7994a0722515648e15.jpg"
+OWNER_IMG= "https://te.legra.ph/file/356874825f8c470f5c281.jpg"
+VEGETA_ADD_IMG="https://te.legra.ph/file/416b2fc1d697a42546c32.jpg"
 
 VALID_WELCOME_FORMATTERS = [
     "first",
