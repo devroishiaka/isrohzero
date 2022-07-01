2c87f201589e22e1e87c5f181499003f23279523
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-01 10:13:55 +0530

    Update gtranslator.py

--- VegetaRobot/modules/gtranslator.py
+++ VegetaRobot/modules/gtranslator.py
@@ -1,31 +1,20 @@
-from gpytranslate import Translator
-from telegram.ext import CommandHandler, CallbackContext
-from telegram import (
-    Message,
-    Chat,
-    User,
-    ParseMode,
-    Update,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-)
-from VegetaRobot import dispatcher, pgram
-from pyrogram import filters
+from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackContext, run_async
+
+from gpytranslate import SyncTranslator
+
+from VegetaRobot import dispatcher
 from VegetaRobot.modules.disable import DisableAbleCommandHandler
-from telegram.ext import run_async
 
 
-__mod_name__ = "Translation"
+trans = SyncTranslator()
 
 
-trans = Translator()
-
-@run_async
-@pgram.on_message(filters.command(["tl", "tr"]))
-async def translate(_, message: Message) -> None:
+def totranslate(update: Update, context: CallbackContext) -> None:
+    message = update.effective_message
     reply_msg = message.reply_to_message
     if not reply_msg:
-        await message.reply_text("Reply to a message to translate it!")
+        message.reply_text("Reply to a message to translate it!")
         return
     if reply_msg.caption:
         to_translate = reply_msg.caption
@@ -37,21 +26,20 @@ async def translate(_, message: Message) -> None:
             source = args.split("//")[0]
             dest = args.split("//")[1]
         else:
-            source = await trans.detect(to_translate)
+            source = trans.detect(to_translate)
             dest = args
     except IndexError:
-        source = await trans.detect(to_translate)
+        source = trans.detect(to_translate)
         dest = "en"
-    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
-    reply = (
-        f"<b>Translated from {source} to {dest}</b>:\n"
+    translation = trans(to_translate,
+                              sourcelang=source, targetlang=dest)
+    reply = f"<b>Translated from {source} to {dest}</b>:\n" \
         f"<code>{translation.text}</code>"
-    )
 
-    await message.reply_text(reply, parse_mode="html")
+    message.reply_text(reply, parse_mode=ParseMode.HTML)
 
 
-def languagess(update: Update, context: CallbackContext) -> None:
+def languages(update: Update, context: CallbackContext) -> None:
     update.effective_message.reply_text(
         "Click on the button below to see the list of supported language codes.",
         reply_markup=InlineKeyboardMarkup(
@@ -59,15 +47,34 @@ def languagess(update: Update, context: CallbackContext) -> None:
                 [
                     InlineKeyboardButton(
                         text="Language codes",
-                        url="https://telegra.ph/Lang-Codes-03-19-3",
-                    ),
+                        url="https://telegra.ph/Lang-Codes-03-19-3"
+                        ),
                 ],
             ],
-            disable_web_page_preview=True,
-        ),
+        disable_web_page_preview=True
     )
+        )
 
+__mod_name__ = "Tʀᴀɴsʟᴀᴛᴏʀ"
 
-LANG_HANDLER = DisableAbleCommandHandler("langs", languages)
+__help__ = """
+ ❍ /tr or /tl (language code) as reply to a long message
+*Example:* 
+ ❍ /tr en*:* translates something to english
+ ❍ /tr hi-en*:* translates hindi to english
+*Language Codes*
+`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
+id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
+"""
 
-dispatcher.add_handler(LANG_HANDLER)
+TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
+
+dispatcher.add_handler(TRANSLATE_HANDLER)
+
+__mod_name__ = "Translator"
+__command_list__ = ["tr", "tl"]
+__handlers__ = [TRANSLATE_HANDLER]
